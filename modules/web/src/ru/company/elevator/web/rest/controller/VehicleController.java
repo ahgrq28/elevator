7abe31c29105c027a793d402b3e0aca082192f6b
Andrey Kabanov <kabanov@haulmont.com>
2021-07-07 16:46:51 +0400

    fix restApi

--- modules/web/src/ru/company/elevator/web/rest/controller/VehicleController.java
+++ modules/web/src/ru/company/elevator/web/rest/controller/VehicleController.java
@@ -1,22 +1,14 @@
 package ru.company.elevator.web.rest.controller;
 
-import com.haulmont.addon.restapi.api.service.filter.data.ResponseInfo;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.company.elevator.dto.VehicleDto;
 import ru.company.elevator.entity.transport.Vehicle;
 import ru.company.elevator.service.VehicleService;
 
 import javax.inject.Inject;
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
